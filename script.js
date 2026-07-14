f68473f40f9a89cdabe6c679ead8526de129ca36
zerodaylearn <aurashoad@gmail.com>
2026-07-14 19:20:37 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,8 @@
-// ZeroDay Recovery
-
-window.addEventListener("load", () => {
-
-const loader = document.getElementById("loader");
-
-loader.style.opacity="0";
-
-setTimeout(()=>{
-
-loader.style.display="none";
-
-},800);
-
-});
-
-const buttons=document.querySelectorAll(".btn,.btn2");
-
-buttons.forEach(btn=>{
-
-btn.addEventListener("mouseenter",()=>{
-
-btn.style.transform="scale(1.05)";
-
-});
-
-btn.addEventListener("mouseleave",()=>{
-
-btn.style.transform="scale(1)";
-
-});
-
+document.addEventListener("DOMContentLoaded", () => {
+  const video = document.getElementById("bgVideo");
+  if (video) {
+    video.play().catch(error => {
+      console.log("Autoplay issue:", error);
+    });
+  }
 });
