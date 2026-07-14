10ad3483926a54439ba7c014fcbb1c0b67733180
zerodaylearn <aurashoad@gmail.com>
2026-07-14 17:33:20 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
+// ZeroDay Recovery
 
+window.addEventListener("load", () => {
+
+const loader = document.getElementById("loader");
+
+loader.style.opacity="0";
+
+setTimeout(()=>{
+
+loader.style.display="none";
+
+},800);
+
+});
+
+const buttons=document.querySelectorAll(".btn,.btn2");
+
+buttons.forEach(btn=>{
+
+btn.addEventListener("mouseenter",()=>{
+
+btn.style.transform="scale(1.05)";
+
+});
+
+btn.addEventListener("mouseleave",()=>{
+
+btn.style.transform="scale(1)";
+
+});
+
+});
